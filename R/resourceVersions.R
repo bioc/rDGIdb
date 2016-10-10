6a2f7425166e5b36aa7319ed77b63952e8ac2a1c
Thomas Thurnherr <thomas.thurnherr@bsse.ethz.ch>
2016-10-10 07:57:23 +0000

    Added function to query resource versions.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/rDGIdb@122123 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/resourceVersions.R
@@ -0,0 +1,28 @@
+getWebsite <- function(url) {
+    getRequest <- GET(url = url)
+    text <- content(getRequest, as = "text", encoding = "ISO-8859-1")
+    return(text)
+}
+
+resourceVersions <- function() {
+    url <- "http://dgidb.genome.wustl.edu/sources/#"
+    text <- getWebsite(url)
+    array <- strsplit(text, '\n')[[1]]
+    interactionId <- grep('class=\'interaction', array)
+    name <- sapply(array[interactionId + 4], function(x) strsplit(x, '<|>', )[[1]][3])
+    version <- array[interactionId + 17]
+    result <- data.frame(cbind(name, version), stringsAsFactors = FALSE)
+    colnames(result) <- c('Name', 'Version')
+    rownames(result) <- 1:nrow(result)
+    result <- result[order(result$Name),]
+    return(result)
+}
+
+# drugBankVersion <- function() {
+#     url <- "http://www.drugbank.ca/about"
+#     text <- getWebsite(url)
+#     array <- strsplit(text, '<|>')[[1]]
+#     grep('DrugBank Version \\d\\.\\d', array, value = TRUE)
+#      
+# }
+
